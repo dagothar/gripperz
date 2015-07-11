b332329ba44301d1e16b1ec9c8fd34e4376bb4a7
dagothar <dagothar@gmail.com>
2015-07-11 23:12:59 +0200

    added a tool to generate random grippers

---
+++ src/tools/generate_grippers.cpp
@@ -0,0 +1,226 @@
+/**
+ * @file generate_grippers.cpp
+ * @author Adam Wolniakowski
+ * @date 11-07-2015
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <rw/math/Math.hpp>
+#include <rw/rw.hpp>
+#include <rw/RobWork.hpp>
+#include <rw/common/Log.hpp>
+#include <rw/common/ExtensionRegistry.hpp>
+#include <rwsim/rwsim.hpp>
+#include <rwsim/loaders/DynamicWorkCellLoader.hpp>
+#include <rw/loaders/model3d/STLFile.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+#include <rwsimlibs/ode/ODEPlugin.hpp>
+
+#include <context/TaskDescription.hpp>
+#include <loaders/TaskDescriptionLoader.hpp>
+#include <models/Gripper.hpp>
+#include <loaders/GripperXMLLoader.hpp>
+#include <grasps/TaskGenerator.hpp>
+#include <simulation/GripperTaskSimulator.hpp>
+#include <simulation/InterferenceSimulator.hpp>
+#include <evaluation/GripperEvaluator.hpp>
+#include <models/MapGripperBuilder.hpp>
+#include <evaluation/GripperObjectiveFunction.hpp>
+#include <evaluation/GripperEvaluationManager.hpp>
+#include <evaluation/GripperEvaluationManagerFactory.hpp>
+#include <optimization/CombineObjectivesFactory.hpp>
+#include <optimization/CombinedFunction.hpp>
+
+
+#define DEBUG rw::common::Log::debugLog()
+#define INFO rw::common::Log::infoLog()
+
+
+using namespace std;
+USE_ROBWORK_NAMESPACE
+using namespace robwork;
+using namespace rw::models;
+using namespace rw::common;
+using namespace rw::loaders;
+using namespace rwlibs::task;
+using namespace rwsim;
+using namespace rwsim::dynamics;
+using namespace rwsim::loaders;
+using namespace rwsim::simulation;
+using namespace boost::program_options;
+namespace po = boost::program_options;
+using namespace gripperz::models;
+using namespace gripperz::context;
+using namespace gripperz::simulation;
+using namespace gripperz::optimization;
+using namespace gripperz::evaluation;
+using namespace gripperz::grasps;
+using namespace gripperz::loaders;
+
+
+string vectorToString(const vector<double>& v) {
+	stringstream sstr;
+	
+	for (unsigned i = 0; i < v.size(); ++i) {
+		sstr << v[i] << ", ";
+	}
+	
+	return sstr.str();
+}
+
+
+int main(int argc, char* argv[]) {
+	/* initialize robwork */
+	Math::seed();
+	RobWork::getInstance()->initialize();
+	Log::log().setLevel(Log::Info);
+	
+	/* parameter bounds */
+	map<MapGripperBuilder::ParameterName, pair<double, double> > bounds;
+	bounds[MapGripperBuilder::Length] = make_pair(0.0, 0.2);
+	bounds[MapGripperBuilder::Width] = make_pair(0.0, 0.05);
+	bounds[MapGripperBuilder::Depth] = make_pair(0.0, 0.05);
+	bounds[MapGripperBuilder::ChfDepth] = make_pair(0.0, 1.0);
+	bounds[MapGripperBuilder::ChfAngle] = make_pair(0.0, 90.0);
+	bounds[MapGripperBuilder::CutDepth] = make_pair(0.0, 0.05);
+	bounds[MapGripperBuilder::CutAngle] = make_pair(0.0, 180.0);
+	bounds[MapGripperBuilder::CutTilt] = make_pair(-90.0, 90.0);
+	bounds[MapGripperBuilder::TcpOffset] = make_pair(0.0, 0.2);
+	bounds[MapGripperBuilder::Jawdist] = make_pair(0.0, 0.05);
+	bounds[MapGripperBuilder::Stroke] = make_pair(0.0, 0.05);
+	bounds[MapGripperBuilder::Force] = make_pair(0.0, 20.0);
+	
+	Q lower(11), upper(11);
+	for (unsigned i = 0; i < 12; ++i) {
+		lower(i) = bounds[static_cast<MapGripperBuilder::ParameterName>(i)].first;
+		upper(i) = bounds[static_cast<MapGripperBuilder::ParameterName>(i)].second;
+	}
+
+	/* options */
+	int cores, ntargets, nrobust;
+	string dwcFilename;
+	string tdFilename;
+	int ngrippers;
+	string name;
+	string samplesFilename;
+	vector<MapGripperBuilder::ParameterName> params{
+		MapGripperBuilder::Length,
+		MapGripperBuilder::Width,
+		MapGripperBuilder::Depth,
+		MapGripperBuilder::ChfDepth,
+		MapGripperBuilder::ChfAngle,
+		MapGripperBuilder::CutDepth,
+		MapGripperBuilder::CutAngle,
+		MapGripperBuilder::CutTilt, 
+		MapGripperBuilder::TcpOffset,
+		MapGripperBuilder::Jawdist,
+		MapGripperBuilder::Stroke,
+		MapGripperBuilder::Force
+	};
+	vector<double> weights{1, 1, 1, 1, 1, 1, 1};
+
+	/* define CLI options */
+	string usage =
+			"This is a script used to generate tasks for a single gripper, simulate them and"
+					" evaluate gripper's performance.\n\n"
+					"Usage:\n"
+					"evaluate-gripper";
+	options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "help message")
+		("cores,c", value<int>(&cores)->default_value(1), "number of threads to use")
+		("ntargets,t", value<int>(&ntargets)->default_value(100), "number of tasks to simulate")
+		("nrobust,r", value<int>(&nrobust)->default_value(100), "number of robustness tasks to generate")
+		("weights,w", value<vector<double> >(&weights)->multitoken(), "7 weights for objectives (0-6)")
+		("dwc", value<string>(&dwcFilename)->required(), "dynamic workcell file")
+		("td", value<string>(&tdFilename)->required(), "task description file")
+		("name,n", value<string>(&name)->required(), "name for grippers")
+		("ngrippers,g", value<int>(&ngrippers)->required(), "number of grippers to generate")
+		("ssamples,s", value<string>(&samplesFilename), "surface samples file");
+	variables_map vm;
+	
+	/* parse CLI */
+	try {
+		store(command_line_parser(argc, argv).options(desc).run(), vm);
+		notify(vm);
+
+		if (vm.count("help")) {
+			cout << usage << endl;
+			cout << desc << endl;
+			return 0;
+		}
+
+	} catch (...) {
+		cout << usage << endl;
+		cout << desc << endl;
+		return -1;
+	}
+
+	/* load data */
+	INFO << "* Loading dwc... ";
+	DynamicWorkCell::Ptr dwc = DynamicWorkCellLoader::load(dwcFilename);
+	INFO << "Loaded." << endl;
+	INFO << "* Loading task description... " << tdFilename << " " << endl;
+	TaskDescription::Ptr td = TaskDescriptionLoader::load(tdFilename, dwc);
+	INFO << "Loaded." << endl;
+
+	vector<SurfaceSample> ssamples;
+	if (vm.count("ssamples")) {
+		INFO << "* Loading samples... ";
+		ssamples = SurfaceSample::loadFromXML(samplesFilename);
+		INFO << "Loaded." << endl;
+	}
+	
+	/* construct objective function */
+	GripperEvaluationManager::Configuration config;
+	config.nOfRobustnessTargets = nrobust;
+	GripperEvaluationManager::Ptr manager = GripperEvaluationManagerFactory::getEvaluationManager(td, config, cores, ssamples);
+	
+	GripperBuilder::Ptr builder = new MapGripperBuilder(ownedPtr(new Gripper), params);
+	MultiObjectiveFunction::Ptr func = new GripperObjectiveFunction(builder, manager);
+	
+	CombineObjectives::Ptr logMethod = CombineObjectivesFactory::make("log", weights);
+	ObjectiveFunction::Ptr objective = new CombinedFunction(func, logMethod);
+	
+	/* generate grippers */
+	unsigned tries = 0;
+	unsigned generated = 0;
+	while (generated < ngrippers) {
+		cout << "# Trying gripper " << ++tries << endl;
+		cout << "# Grippers generated so far: " << generated << endl;
+		
+		/* randomize parameters */
+		Q qparams = Math::ranQ(lower, upper);
+		vector<double> vparams = Math::toStdVector(qparams, 12);
+		
+		/* test gripper */
+		double q = (*objective)(vparams);
+		cout << "Gripper quality= " << q << endl;
+		
+		if (q > 0.0) {
+			++generated;
+			
+			stringstream sstr;
+			sstr << name << generated;
+			Gripper::Ptr grp = builder->parametersToGripper(vparams);
+			grp->setName(sstr.str());
+			GripperXMLLoader::save(grp, sstr.str() + ".grp.xml");
+			
+			cout << "Gripper OK" << endl;
+		} else {
+			cout << "Gripper NOT OK" << endl;
+		}
+	}
+	
+	cout << "Finished. Generated " << generated << " grippers with " << tries << " tries." << endl;
+
+	return 0;
+}
