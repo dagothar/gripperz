425f109ce2246e27f99d0e33d6b268c75d123f54
dagothar <dagothar@gmail.com>
2015-07-15 23:44:10 +0200

    modified landscape plot: reduced ymax and moved legend to the right

--- scripts/landscape.R
+++ scripts/landscape.R
@@ -9,6 +9,7 @@ data_file = paste(name, ".csv", sep="")
 title=paste(name)
 lwd = 2
 ptsize = 14
+ymax = 1
 
 # read data
 data = read.csv(data_file, skip=1)
@@ -26,24 +27,32 @@ qlog = as.numeric(unlist(data[10]))
 
 # draw plot
 png(filename=plot_file, width=1024, height=768, units="px", pointsize=ptsize)
-plot(suc~val, type='l', xlab='value', ylab='q', col='red', main=title, lwd=lwd, ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+
+# this is to make border for legend
+par(oma = c(1, 1, 1, 6))
+
+# actual plot
+plot(suc~val, type='l', xlab='value', ylab='q', col='red', main=title, lwd=lwd, ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(rob~val, type='l', col='darkred', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(rob~val, type='l', col='darkred', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(cov~val, type='l', col='green', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(cov~val, type='l', col='green', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(wre~val, type='l', col='blue', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(wre~val, type='l', col='blue', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(ali~val, type='l', col='darkorange', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(ali~val, type='l', col='darkorange', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(str~val, type='l', col='violet', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(str~val, type='l', col='violet', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(vol~val, type='l', col='cyan', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(vol~val, type='l', col='cyan', lwd=lwd, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(qsum~val, type='l', col='black', lwd=3, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), lty=2, xaxs="i", yaxs="i")
+plot(qsum~val, type='l', col='black', lwd=3, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), lty=2, xaxs="i", yaxs="i")
 par(new=TRUE)
-plot(qlog~val, type='l', col='black', lwd=3, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, 1.4)), xaxs="i", yaxs="i")
+plot(qlog~val, type='l', col='black', lwd=3, axes = FALSE, xlab = "", ylab = "", ylim=range(c(0, ymax)), xaxs="i", yaxs="i")
 par(new=F)
 
-legend("topright", legend=c("success", "robustness", "alignment", "coverage", "wrench", "stress", "volume", "average", "Q"), col=c("red", "darkred", "darkorange", "green", "blue", "violet", "cyan", "black", "black"), lty=c(1, 1, 1, 1, 1, 1, 1, 2, 1), lwd=lwd)
+par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
+legend("right", legend=c("success", "robustness", "alignment", "coverage", "wrench", "stress", "volume", "average", "Q"), col=c("red", "darkred", "darkorange", "green", "blue", "violet", "cyan", "black", "black"), lty=c(1, 1, 1, 1, 1, 1, 1, 2, 1), lwd=lwd, xpd = TRUE, inset = c(0, 0), bty='n')
+
 dev.off()
