462e9f2ab4b3a70d3c44a9415ab6c70800cf23d5
dagothar <dagothar@gmail.com>
2015-09-03 20:53:54 +0200

    modified gripper generation script

--- src/tools/generate_grippers.cpp
+++ src/tools/generate_grippers.cpp
@@ -98,12 +98,6 @@ int main(int argc, char* argv[]) {
 	bounds[MapGripperBuilder::Jawdist] = make_pair(0.0, 0.05);
 	bounds[MapGripperBuilder::Stroke] = make_pair(0.0, 0.05);
 	bounds[MapGripperBuilder::Force] = make_pair(0.0, 20.0);
-	
-	Q lower(12), upper(12);
-	for (unsigned i = 0; i < 12; ++i) {
-		lower(i) = bounds[static_cast<MapGripperBuilder::ParameterName>(i)].first;
-		upper(i) = bounds[static_cast<MapGripperBuilder::ParameterName>(i)].second;
-	}
 
 	/* options */
 	int cores, ntargets, nrobust;
@@ -113,21 +107,9 @@ int main(int argc, char* argv[]) {
 	int last = 0;
 	string name;
 	string samplesFilename;
-	vector<MapGripperBuilder::ParameterName> params{
-		MapGripperBuilder::Length,
-		MapGripperBuilder::Width,
-		MapGripperBuilder::Depth,
-		MapGripperBuilder::ChfDepth,
-		MapGripperBuilder::ChfAngle,
-		MapGripperBuilder::CutDepth,
-		MapGripperBuilder::CutAngle,
-		MapGripperBuilder::CutTilt, 
-		MapGripperBuilder::TcpOffset,
-		MapGripperBuilder::Jawdist,
-		MapGripperBuilder::Stroke,
-		MapGripperBuilder::Force
-	};
+	string gripper_filename;
 	vector<double> weights{1, 1, 1, 1, 1, 1, 1};
+	vector<int> parameters{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
 	/* define CLI options */
 	string usage =
@@ -142,10 +124,12 @@ int main(int argc, char* argv[]) {
 		("ntargets,t", value<int>(&ntargets)->default_value(100), "number of tasks to simulate")
 		("nrobust,r", value<int>(&nrobust)->default_value(100), "number of robustness tasks to generate")
 		("weights,w", value<vector<double> >(&weights)->multitoken(), "7 weights for objectives (0-6)")
+		("parameters,p", value<vector<int> >(&parameters)->multitoken(), "12 parameters to optimize (0-11)")
+		("gripper,g", value<string>(&gripper_filename)->required(), "gripper file")
 		("dwc", value<string>(&dwcFilename)->required(), "dynamic workcell file")
 		("td", value<string>(&tdFilename)->required(), "task description file")
-		("name,n", value<string>(&name)->required(), "name for grippers")
-		("ngrippers,g", value<int>(&ngrippers)->required(), "number of grippers to generate")
+		("name", value<string>(&name)->required(), "name for grippers")
+		("ngrippers,n", value<int>(&ngrippers)->required(), "number of grippers to generate")
 		("last,l", value<int>(&last)->default_value(0), "last generated index (to continue after crash)")
 		("ssamples,s", value<string>(&samplesFilename), "surface samples file");
 	variables_map vm;
@@ -174,6 +158,21 @@ int main(int argc, char* argv[]) {
 	INFO << "* Loading task description... " << tdFilename << " " << endl;
 	TaskDescription::Ptr td = TaskDescriptionLoader::load(tdFilename, dwc);
 	INFO << "Loaded." << endl;
+	INFO << "* Loading gripper... " << gripper_filename;
+	Gripper::Ptr gripper = GripperXMLLoader::load(gripper_filename);
+	INFO << " Loaded." << endl;
+	
+	/* parameters */
+	vector<MapGripperBuilder::ParameterName> params;
+	BOOST_FOREACH (int id, parameters) {
+		params.push_back(static_cast<MapGripperBuilder::ParameterName>(id));
+	}
+	
+	Q lower(parameters.size()), upper(parameters.size());
+	for (unsigned i = 0; i < parameters.size(); ++i) {
+		lower(i) = bounds[params[i]].first;
+		upper(i) = bounds[params[i]].second;
+	}
 
 	vector<SurfaceSample> ssamples;
 	if (vm.count("ssamples")) {
@@ -189,10 +188,10 @@ int main(int argc, char* argv[]) {
 	GripperEvaluator::Ptr evaluator = new ConstrainedGripperEvaluator(td, td->getAlignmentCalculator());
 	manager->setEvaluator(evaluator);
 	
-	GripperBuilder::Ptr builder = new MapGripperBuilder(ownedPtr(new Gripper), params);
+	GripperBuilder::Ptr builder = new MapGripperBuilder(gripper, params);
 	GripperObjectiveFunction::Ptr func = new GripperObjectiveFunction(builder, manager);
 	
-	CombineObjectives::Ptr logMethod = CombineObjectivesFactory::make("log", weights);
+	CombineObjectives::Ptr logMethod = CombineObjectivesFactory::make("product", weights);
 	ObjectiveFunction::Ptr objective = new CombinedFunction(func, logMethod);
 	
 	/* generate grippers */
@@ -204,7 +203,7 @@ int main(int argc, char* argv[]) {
 		
 		/* randomize parameters */
 		Q qparams = Math::ranQ(lower, upper);
-		vector<double> vparams = Math::toStdVector(qparams, 12);
+		vector<double> vparams = Math::toStdVector(qparams, params.size());
 		
 		/* test gripper */
 		double q = 0;
@@ -219,6 +218,7 @@ int main(int argc, char* argv[]) {
 		sstr << name << "_" << tries;
 		Gripper::Ptr grp = func->getLastGripper();
 		grp->setName(sstr.str());
+		grp->getQuality().quality = q;
 		
 		if (q > 0.0) {
 			++generated;
