a8bc8abbfcfda3dbe4fdeedfdca18429ac559d4c
dagothar <dagothar@gmail.com>
2015-10-07 20:00:07 +0200

    process manager

---
+++ src/process/EvaluationProcessManager.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   EvaluationProcessManager.cpp
+ * Author: dagothar
+ * 
+ * Created on October 7, 2015, 3:39 PM
+ */
+
+#include "EvaluationProcessManager.hpp"
+
+using namespace gripperz::process;
+
+EvaluationProcessManager::EvaluationProcessManager() {
+}
+
+EvaluationProcessManager::EvaluationProcessManager(const EvaluationProcessManager& orig) {
+}
+
+EvaluationProcessManager::~EvaluationProcessManager() {
+}
+
---
+++ src/process/EvaluationProcessManager.hpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   EvaluationProcessManager.hpp
+ * Author: dagothar
+ *
+ * Created on October 7, 2015, 3:39 PM
+ */
+
+#ifndef EVALUATIONPROCESSMANAGER_HPP
+#define	EVALUATIONPROCESSMANAGER_HPP
+
+
+namespace gripperz {
+    namespace process {
+
+        class EvaluationProcessManager {
+        public:
+            EvaluationProcessManager();
+            EvaluationProcessManager(const EvaluationProcessManager& orig);
+            virtual ~EvaluationProcessManager();
+        private:
+
+        };
+
+    }
+}
+
+#endif	/* EVALUATIONPROCESSMANAGER_HPP */
+
