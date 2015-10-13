433d1a3a6ef0642142b78667f816e39c9179f853
dagothar <dagothar@gmail.com>
2015-10-13 11:20:21 +0200

    quality index calculator

---
+++ src/evaluation/Types.hpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   Types.hpp
+ * Author: dagothar
+ *
+ * Created on October 13, 2015, 11:14 AM
+ */
+
+#ifndef TYPES_HPP
+#define	TYPES_HPP
+
+#include <string>
+
+namespace gripperz {
+    namespace evaluation {
+        
+        //! A type for the quality index
+        typedef std::pair<std::string, double> QualityIndex;
+    }
+}
+
+#endif	/* TYPES_HPP */
+
