abafe05f0201ea716c46f88c71ad6a8db1ffd30b
dagothar <dagothar@gmail.com>
2015-10-15 19:56:57 +0200

    ModifiedWorkcellGripper stub

---
+++ src/models/ModifiedWorkcellGripper.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   ModifiedWorkcellGripper.cpp
+ * Author: dagothar
+ * 
+ * Created on October 15, 2015, 6:57 PM
+ */
+
+#include "ModifiedWorkcellGripper.hpp"
+
+using namespace gripperz::models;
+
+ModifiedWorkcellGripper::ModifiedWorkcellGripper(const std::string& name) :
+WorkcellGripper(name) {
+}
+
+ModifiedWorkcellGripper::~ModifiedWorkcellGripper() {
+}
+
