19443cbdf808cd6357025f0cc1b159319cc0bb90
dagothar <dagothar@gmail.com>
2015-10-20 13:59:51 +0200

    implemented success calculator

--- src/evaluation/calculators/SuccessIndexCalculator.cpp
+++ src/evaluation/calculators/SuccessIndexCalculator.cpp
@@ -6,11 +6,18 @@
  */
 
 #include "SuccessIndexCalculator.hpp"
+#include <rwlibs/task/GraspTask.hpp>
+#include <grasps/GraspStatistics.hpp>
+
+#define DEBUG rw::common::Log::debugLog()
+#define INFO rw::common::Log::infoLog()
 
 using namespace std;
 using namespace gripperz::models;
+using namespace gripperz::grasps;
 using namespace gripperz::evaluation;
 using namespace gripperz::evaluation::calculators;
+using namespace rwlibs::task;
 
 SuccessIndexCalculator::SuccessIndexCalculator() {
 }
@@ -20,7 +27,29 @@ SuccessIndexCalculator::~SuccessIndexCalculator() {
 
 QualityIndexValue SuccessIndexCalculator::calculate(models::OldGripper::Ptr gripper, grasps::Grasps grasps) {
 
-    //TODO: implement
-    
-    return 0;
+    DEBUG << "CALCULATING SUCCESS - " << endl;
+    vector<pair<class GraspSubTask*, class GraspTarget*> > allTargets = grasps->getAllTargets();
+
+    int nAllTargets = allTargets.size();
+    int successes = GraspStatistics::countGraspsWithStatus(grasps, GraspResult::Success);
+    int filtered = GraspStatistics::countGraspsWithStatus(grasps, GraspResult::Filtered);
+    int failures = GraspStatistics::countGraspsWithStatus(grasps, GraspResult::SimulationFailure);
+    int samples = GraspStatistics::countGraspsWithStatus(grasps, GraspResult::Skip);
+
+    DEBUG << "alltargets= " << nAllTargets << endl;
+    DEBUG << "successes= " << successes << endl;
+    DEBUG << "filtered= " << filtered << endl;
+    DEBUG << "failures= " << failures << endl;
+    DEBUG << "samples= " << samples << endl;
+
+    double validTasks = nAllTargets - filtered - failures - samples;
+
+    if (validTasks == 0) {
+        RW_WARN("No valid tasks");
+        return 0.0;
+    }
+
+    double successIndex = 1.0 * successes / validTasks;
+
+    return successIndex;
 }
