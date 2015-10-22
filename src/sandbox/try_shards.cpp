472878d3165512ee355d943c5d55b4e0711f92a5
dagothar <dagothar@gmail.com>
2015-10-22 21:05:22 +0200

    ShardMaker

--- src/sandbox/try_shards.cpp
+++ src/sandbox/try_shards.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <string>
+#include <rw/common/Ptr.hpp>
 #include "shards.hpp"
 
 using namespace std;
+using namespace rw::common;
 using namespace gripperz::shards;
 
+class Person : public ShardMaker<Person> {
+public:
+    typedef rw::common::Ptr<Person> Ptr;
+
+    Person() {
+    }
+
+    void setName(const string& name) {
+        _name = name;
+    }
+    
+    string getName() const { return _name; }
+    
+
+    virtual Shard doMakeShard(ShardContainer& container) const {
+        string name = container.getShard("string").cast<string>();
+        
+        Person::Ptr p = ownedPtr(new Person());
+        p->setName(name);
+        
+        return Shard("id", p);
+    }
+    
+private:
+    string _name;
+};
+
 int main(int argc, char* argv[]) {
+    ShardContainer container;
+    
+    container.addShard("string", Shard("name", string("adam")));
+    container.addShard("PersonPtr", Person::makeShard(container));
+    
+    Person::Ptr p = container.getShard("PersonPtr").cast<Person::Ptr>();
+    cout << p->getName() << endl;
 
     return 0;
 }
--- src/shards.hpp
+++ src/shards.hpp
@@ -16,3 +16,4 @@ namespace gripperz {
 #include "shards/Shard.hpp"
 #include "shards/ShardContainer.hpp"
 #include "shards/ShardExceptions.hpp"
+#include "shards/ShardMaker.hpp"
