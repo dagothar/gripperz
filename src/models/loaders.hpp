7a75902b38f33f104c4e2ff7eea5b61e8359dd35
dagothar <dagothar@gmail.com>
2015-10-23 12:13:11 +0200

    added Loader interface and the Gripper loader stub

---
+++ src/models/loaders.hpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   loaders.hpp
+ * Author: dagothar
+ *
+ * Created on October 23, 2015, 11:41 AM
+ */
+
+#pragma once
+
+namespace gripperz {
+    namespace models {
+        namespace loaders {
+            
+        }
+    }
+}
---
+++ src/models/loaders/GripperLoader.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   GripperLoader.cpp
+ * Author: dagothar
+ * 
+ * Created on October 23, 2015, 11:44 AM
+ */
+
+#include "GripperLoader.hpp"
+
+using namespace NAMESPACE1::NAMESPACE2;
+
+GripperLoader::GripperLoader() {
+}
+
+GripperLoader::~GripperLoader() {
+}
+
