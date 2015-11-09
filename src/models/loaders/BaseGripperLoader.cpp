f47107c6d6a824974eabf9a4faa416c8b88e4ccf
dagothar <dagothar@gmail.com>
2015-11-09 20:00:29 +0100

    gripper loaders now also save quality

--- src/models/loaders/BaseGripperLoader.cpp
+++ src/models/loaders/BaseGripperLoader.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "BaseGripperLoader.hpp"
+#include "GripperQualityLoader.hpp"
+#include "models/GripperQualityFactory.hpp"
 
 using namespace std;
 using namespace rw::common;
@@ -44,6 +46,15 @@ Gripper::Ptr BaseGripperLoader::read(const boost::property_tree::ptree& tree) {
 
     const ptree& data = tree.get_child("data");
     readData(data, gripper);
+    
+    boost::optional<const ptree&> q_node = tree.get_child_optional("quality");
+    if (q_node) {
+        GripperQualityLoader::Ptr q_loader = new GripperQualityLoader();
+        GripperQuality::Ptr quality = q_loader->read(*q_node);
+        gripper->setQuality(quality);
+    } else {
+        gripper->setQuality(GripperQualityFactory::makeGripperQuality());
+    }
 
     return gripper;
 }
@@ -59,6 +70,10 @@ pair<string, ptree> BaseGripperLoader::write(Gripper::Ptr gripper) {
     tree.put("data.graspControllerId", gripper->getGraspControllerId());
     tree.put("data.leftFingerId", gripper->getLeftFingerId());
     tree.put("data.rightFingerId", gripper->getRightFingerId());
+    
+    GripperQualityLoader::Ptr q_loader = new GripperQualityLoader();
+    auto q_node = q_loader->write(gripper->getQuality());
+    tree.add_child("quality", q_node.second);
 
     return make_pair("gripper", tree);
 }
