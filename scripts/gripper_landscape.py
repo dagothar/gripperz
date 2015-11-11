43a7ffa3bcc3abba6eefde4936e3da3bbceeaf62
dagothar <dagothar@gmail.com>
2015-11-11 14:51:16 +0100

    moved landscape test to gripper landscape

--- scripts/gripper_landscape.py
+++ scripts/gripper_landscape.py
@@ -1,152 +1,120 @@
 #!/usr/bin/python
 
+# Example:
+#	./gripper_landscape.py --dwc /home/dothar/grippers/scenes/rotor/Scene.dwc.xml --td /home/dagothar/grippers/scenes/rotor/task3.td.xml --gripper ~/gripperz-dev/data/test/standard_gripper.xml -p cutdepth --grasps ~/gripperz-dev/data/tasks/the_grasp.rwtask.xml --res 100 --ngrasps 100 --bounds 0.0 0.025 --alir 0.001 --seed 0
+
 
 import argparse
-import os
-import subprocess
-import xml.etree.ElementTree as ET
 import numpy
 import signal
+import os
 import sys
+import xml.etree.ElementTree as ET
 
 
-# CONFIGURATION
-EVALUATION_COMMAND = '${{GRIPPERZ_ROOT}}/bin/gripper_evaluate -c {cores} -g {simulations} -r {robustness} -v "{values}" --dwc {dwc} --td {td} --samples {samples} --name {name} 1>/dev/null 2>/dev/null'
-P_NAMES = ['length', 'width', 'depth', 'chfdepth', 'chfangle', 'cutdepth', 'cutangle', 'tilt', 'tcp', 'jawdist', 'stroke', 'force']
-P_RANGES = [
-	(0, 0.2),
-	(0, 0.05),
-	(0, 0.05),
-	(0, 1),
-	(0, 90),
-	(0, 0.05),
-	(0, 180),
-	(-180, 180),
-	(0, 0.2),
-	(0, 0.05),
-	(0, 0.05),
-	(0, 50)
-]
+CMD = '/home/dagothar/gripperz-dev/bin/evaluate_gripper --dwc {dwc} --td {td} --gripper {gripper} -p {parameter} -v "{value}" -t {threads} -n {ngrasps} -r {result} --aliR {alir} --seed {seed}'
 
 
 def signal_handler(signal, frame):
 	print('Stopping')
 	sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def init():
-	"""
-	Parses command line and creates configuration data
-	"""
+def main():
+	
 	parser = argparse.ArgumentParser(description="Generates gripper landscape data")
-	parser.add_argument("--scene", metavar='SCENE', required=True, help="which scene to use")
-	parser.add_argument("--task", metavar='TASK', required=True, help="which task to use")
-	parser.add_argument("-p", "--parameters", nargs='+', metavar='PARAMETERS', default=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], help="parameters to landscape")
-	parser.add_argument("-i", "--seed", nargs='+', metavar='INITIAL', default=[0.1, 0.025, 0.02, 0.5, 45, 0.01, 90, 0, 0.025, 0, 0.05, 25], help="initial gripper configuration (12 values)")
-	parser.add_argument("--res", metavar='RES', default=20, help="landscape resolution")
-	parser.add_argument("-n", "--targets", metavar='TARGETS', default=100, help="number of simulations per evaluation")
-	parser.add_argument("-r", "--robust", metavar='ROBUST', default=100, help="number of robustness simulations per evaluation")
-	parser.add_argument("-c", "--cores", metavar='CORES', default=1, help="number of cores to use")
-
+	parser.add_argument("--dwc", metavar='DWC', required=True, help="scene dwc file")
+	parser.add_argument("--td", metavar='TD', required=True, help="scene td file")
+	parser.add_argument("--gripper", metavar='GRP', required=True, help="gripper xml file")
+	parser.add_argument("--grasps", metavar='RWTASK', default='', help="grasps in RW xml format")
+	parser.add_argument("--save_grasps", dest='save_grasps', action='store_true', help="save grasps in RW xml format?")
+	parser.add_argument("--parameter", "-p", metavar='PARAMETER', required=True, help="which parameter to landscape")
+	parser.add_argument("--res", metavar='RES', default=100, help="landscape resolution")
+	parser.add_argument("--ngrasps", "-n", metavar='NGRASPS', default=100, help="# of grasps per evaluation")
+	parser.add_argument("--threads", "-t", metavar='THREADS', default=4, help="# of threads to use")
+	parser.add_argument("--bounds", "-b", metavar='BOUNDS', nargs=2, required=True, help="parameter bounds")
+	parser.add_argument("--alir", metavar='ALIR', default=0.01, help="alignment filtering radius")
+	parser.add_argument("--seed", metavar='SEED', default=0, help="RNG seed")
+	parser.set_defaults(save_grasps=False)
 	args = parser.parse_args()
 	
-	return {
-		'scene': args.scene,
-		'task': args.task,
-		'parameters': [int(p) for p in args.parameters],
-		'seed': [float(v) for v in args.seed],
-		'res': int(args.res),
-		'targets': int(args.targets),
-		'robust': int(args.robust),
-		'cores': int(args.cores)
-	}
-
-
-def extract_quality(filename):
-	"""
-	Extracts quality values from evaluated gripper file
-	"""
+	DWC = args.dwc
+	TD = args.td
+	GRIPPER = args.gripper
+	GRASPS = args.grasps
+	PARAM = args.parameter
+	RES = int(args.res)
+	NGRASPS = int(args.ngrasps)
+	THREADS = int(args.threads)
+	BOUNDS = [float(b) for b in args.bounds]
+	ALIR = float(args.alir)
+	SEED = int(args.seed)
+	SAVE = args.save_grasps
+		
+	data = open(PARAM + '.csv', 'w')
+	data.write(PARAM + ", success, alignment, coverage, wrench, stress, volume, qsum, qlog\n");
 	
-	tree = ET.parse(filename)
-	root = tree.getroot()
-	
-	success = float(root.iter('success').next().text)
-	robustness = float(root.iter('robustness').next().text)
-	alignment = float(root.iter('alignment').next().text)
-	coverage = float(root.iter('coverage').next().text)
-	wrench = float(root.iter('wrench').next().text)
-	stress = float(root.iter('stress').next().text)
-	volume = float(root.iter('volume').next().text)
-	qlog = float(root.iter('quality').next().text)
-	
-	qsum = (success + robustness + alignment + coverage + wrench + stress + volume) / 7.0
-	
-	return [success, robustness, alignment, coverage, wrench, stress, volume, qsum, qlog]
-
-
-def evaluate(values, scene, task, targets, robust, cores, n=10):
-	"""
-	Evaluates gripper design given by provided parameters.
-	Tries a number of times in case a segmentation fault occurs.
-	"""
-	
-	cmd = EVALUATION_COMMAND.format(
-		cores = cores,
-		simulations = targets,
-		robustness = robust,
-		values = " ".join(str(v) for v in values),
-		dwc = "${GRIPPERS_ROOT}/scenes/" + scene + "/Scene.dwc.xml",
-		td = "${GRIPPERS_ROOT}/scenes/" + scene + "/task" + task + ".td.xml",
-		samples = "${GRIPPERS_ROOT}/scenes/" + scene + "/samples" + task + ".xml",
-		name = ".gripper"
-	)
-	
-	quality = [0, 0, 0, 0, 0, 0, 0, 0]
-	
-	tries = 0
-	while tries < n:
+	i = 0
+	for v in numpy.linspace(BOUNDS[0], BOUNDS[1], RES+1, endpoint=True):
+		print "Evaluating " + PARAM + " = " + str(v)
+		result = PARAM + '_' + str(i) + '.grp.xml'
+		tasks = PARAM + '_' + str(i) + '.rwtask.xml'
+		
+		# construct command
+		cmd = CMD.format(
+			dwc = DWC,
+			td = TD,
+			gripper = GRIPPER,
+			parameter = PARAM,
+			value = str(v),
+			threads = THREADS,
+			ngrasps = NGRASPS,
+			result = result,
+			alir = ALIR,
+			seed = SEED
+		)
+		
+		if (TD):
+			cmd += ' --grasps {grasps}'.format(grasps = GRASPS)
+			
+		if (SAVE):
+			cmd += ' --save_grasps {tasks}'.format(tasks = tasks)
+		
+		# evaluate
 		ret = os.system(cmd)
 		
 		if (ret == 2): # ctrl - c
 			sys.exit(0)
-		
-		if (ret == 0):
-			quality = extract_quality(".gripper.grp.xml")
-			break
-		else:
-			print "FAILED"
-			tries += 1
-	
-	return quality
-	
-
-def main():
-	args = init()
-	
-	for p in args['parameters']:
-		name = P_NAMES[p]
-		print "* Evaluating landscape for " + name
-		
-		data = open(name + '.csv', 'w')
-		data.write("# " + name + ", success, robustness, alignment, coverage, wrench, stress, volume, qsum, qlog\n");
-		
-		bounds = P_RANGES[p]
-		for v in numpy.linspace(bounds[0], bounds[1], args['res']+1, endpoint=True):
-			print "Evaluating " + name + "= " + str(v)
 			
-			params = args['seed']
-			params[p] = v
-			
-			quality = evaluate(params, args['scene'], args['task'], args['targets'], args['robust'], args['cores'])
-			print str(v) + ", " + ", ".join(str(x) for x in quality)
-			data.write(str(v) + ", " + ", ".join(str(x) for x in quality) + "\n")
-			data.flush()
+		if (ret != 0):
+			continue
 		
-		data.close()
+		# parse quality
+		tree = ET.parse(result)
+		root = tree.getroot()
 		
+		success = float(root.find('.//index[@name="success"]').text)
+		alignment = float(root.find('.//index[@name="alignment"]').text)
+		coverage = float(root.find('.//index[@name="coverage"]').text)
+		wrench = float(root.find('.//index[@name="wrench"]').text)
+		stress = float(root.find('.//index[@name="stress"]').text)
+		volume = float(root.find('.//index[@name="volume"]').text)
+		qlog = float(root.find('.//index[@name="Q"]').text)
+		qsum = (success + alignment + coverage + wrench + stress + volume) / 6.0
+		
+		record = [v, success, alignment, coverage, wrench, stress, volume, qsum, qlog]
+		print record
+		
+		data.write(", ".join(str(x) for x in record) + "\n")
+		data.flush()
+		
+		i += 1
+		
+	data.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
